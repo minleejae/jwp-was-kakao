bb528b572e932ddf79dea9d4e7b1d0af0e41d02e
minleejae <mmj9808@naver.com>
2024-04-15 15:08:03 +0900

    refactor: RequestCommand httpRequest 재사용 하도록 변경

--- src/main/java/http/commands/HttpRequestCommand.java
+++ src/main/java/http/commands/HttpRequestCommand.java
@@ -1,10 +1,11 @@
 package http.commands;
 
+import http.request.HttpRequest;
 import http.response.HttpResponse;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public interface HttpRequestCommand {
-    HttpResponse handle() throws IOException, URISyntaxException;
+    HttpResponse handle(HttpRequest httpRequest) throws IOException, URISyntaxException;
 }
