212d43806119ec443276d029d994e4f170ea70ed
minleejae <mmj9808@naver.com>
2024-04-17 16:49:28 +0900

    feat: 회원가입 중복된 유저 검증 추가

--- src/main/java/webserver/commands/CreateUserCommand.java
+++ src/main/java/webserver/commands/CreateUserCommand.java
@@ -18,14 +18,20 @@ public class CreateUserCommand implements HttpRequestCommand {
         String requestBody = httpRequest.getBody();
         QueryParams queryParams = QueryParams.of(requestBody);
 
-        User user = new User(
+        User user = DataBase.findUserById(queryParams.get("userId"));
+
+        if (user != null) {
+            throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
+        }
+
+        User newUser = new User(
                 queryParams.get("userId"),
                 queryParams.get("password"),
                 queryParams.get("name"),
                 queryParams.get("email")
         );
 
-        DataBase.addUser(user);
+        DataBase.addUser(newUser);
 
         return createRedirectResponse("/index.html");
     }
